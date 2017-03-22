11929a7e6f1a446319264b6cb824c643f59d419c
David-Rodden <davidrodden273@gmail.com>
2017-03-22 16:51:25 -0700

    Added tmx and connected GameScreen to Platformer main class

--- desktop/src/com/mygdx/learning/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/learning/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.learning.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.learning.LearningAlgorithm;
+import com.mygdx.learning.Platformer;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new LearningAlgorithm(), config);
+        new LwjglApplication(new Platformer(), config);
     }
 }

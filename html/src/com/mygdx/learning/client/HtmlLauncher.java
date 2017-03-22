628b39afcea45cb519b91bddcd2276004fac7866
David-Rodden <davidrodden273@gmail.com>
2017-03-22 03:05:17 -0700

    Changing project to use libgdx instead of purely lwjgl3

---
+++ html/src/com/mygdx/learning/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.learning.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.learning.LearningAlgorithm;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LearningAlgorithm();
+        }
+}
